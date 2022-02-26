5a75fcbb9b86b717f8a7e753c7b1790dd79be9e7
Dogey11 <69096657+Dogey11@users.noreply.github.com>
2022-02-25 23:47:12 -0800

    files

---
+++ dsplay.d
@@ -0,0 +1,17 @@
+module dsplay;
+
+import windows.winuser;
+
+int getScreenWidth() {
+	int x = GetSystemMetrics(SM_CXSCREEN);
+	x = x / 4;
+	x = x * 5;
+	return x;
+}
+
+int getScreenHeight() {
+	int y = GetSystemMetrics(SM_CYSCREEN);
+	y = y / 4;
+	y = y * 5;
+	return y;
+}
